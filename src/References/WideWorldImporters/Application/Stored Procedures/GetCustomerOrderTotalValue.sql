5f6850d3d514a032eba5828efd9207a0ea9d08ff
Kamil Nowinski <kamil@nowinski.net>
2020-08-09 00:02:18 +0100

    WWIExt

---
+++ src/References/WideWorldImporters/Application/Stored Procedures/GetCustomerOrderTotalValue.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[GetCustomerOrderTotalValue]
+	@CustomerId int
+AS
+	DECLARE @CustomerKey int
+	SELECT @CustomerKey = [Customer Key] 
+	FROM [$(srv1020)].[$(WideWorldImportersDW)].Dimension.Customer 
+	WHERE [WWI Customer ID] = @CustomerId;
+
+	SELECT @CustomerId, SUM([Total Excluding Tax]) AS TotalValueExTax
+	FROM [$(srv1020)].[$(WideWorldImportersDW)].Fact.[Order]
+	WHERE [Customer Key] = @CustomerKey;
+
+RETURN 0
