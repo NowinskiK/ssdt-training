77b6ba8fd98d32c5b162577aadc3effdb5161b40
Kamil Nowinski <kamil@nowinski.net>
2020-09-06 19:02:34 +0100

    PreDeployment for DimProduct fixed

--- src/PrePostDeployment/ContosoRetailDW/Scripts/PreDeployment.sql
+++ src/PrePostDeployment/ContosoRetailDW/Scripts/PreDeployment.sql
@@ -10,23 +10,34 @@
 --------------------------------------------------------------------------------------
 */
 
-UPDATE [dbo].[DimProduct]
-SET [StockTypeID] = -1
-WHERE [StockTypeID] IS NULL;
-GO
-
-WITH cteStockTypes as (
-	SELECT 1 as [Id], 'High' as [Name] UNION ALL
-	SELECT 2, 'Mid' UNION ALL
-	SELECT 3, 'Low' UNION ALL
-	SELECT -1, 'Unknown' 
+IF EXISTS (
+  SELECT *
+	FROM sys.tables
+	JOIN sys.schemas
+	  ON sys.tables.schema_id = sys.schemas.schema_id
+   WHERE sys.schemas.name = N'dbo'
+	 AND sys.tables.name = N'DimProduct'
 )
+BEGIN
+	UPDATE [dbo].[DimProduct]
+	SET [StockTypeID] = -1
+	WHERE [StockTypeID] IS NULL;
 
-UPDATE P
-SET [StockTypeName] = ST.[Name]
-FROM [dbo].[DimProduct] as P 
-JOIN cteStockTypes ST ON P.StockTypeID = ST.Id
-WHERE P.[StockTypeName] IS NULL
+	WITH cteStockTypes as (
+		SELECT 1 as [Id], 'High' as [Name] UNION ALL
+		SELECT 2, 'Mid' UNION ALL
+		SELECT 3, 'Low' UNION ALL
+		SELECT -1, 'Unknown' 
+	)
+
+	UPDATE P
+	SET [StockTypeName] = ST.[Name]
+	FROM [dbo].[DimProduct] as P 
+	JOIN cteStockTypes ST ON P.StockTypeID = ST.Id
+	WHERE P.[StockTypeName] IS NULL
+END
+
+
 GO
 
 
