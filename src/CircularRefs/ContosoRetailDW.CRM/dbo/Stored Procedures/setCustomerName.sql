c177e22875077403176bbce7d9366f6bdd924c7f
Kamil Nowinski <kamil@nowinski.net>
2020-11-20 14:28:06 +0000

    Circular Reference solution

---
+++ src/CircularRefs/ContosoRetailDW.CRM/dbo/Stored Procedures/setCustomerName.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[setCustomerName]
+	@customerId INT,
+	@FirstName NVARCHAR(50),
+	@Surname NVARCHAR(50)
+AS
+	UPDATE [$(CRM)].dbo.Customer
+	SET 
+		[FirstName] = @FirstName, 
+		[Surname] = @Surname
+	WHERE [CustomerId] = @customerId;
+
+RETURN 0
